9ed8a17e3a3ad478ddef9a1ef200bd8c31302419
Yuan Zhou <yuanzhou@yuans-macbook-air.local>
2017-01-04 13:07:44 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Yuan Zhou on 1/4/17.
+//  Copyright © 2017 Yuan Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
